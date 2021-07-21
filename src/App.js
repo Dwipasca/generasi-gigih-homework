2ccb6eb9be3aa6139f746e772a6dfacf17d83b6c
Dwi Pasca <dpnursanti@gmail.com>
2021-07-21 13:07:12 +0800

    remake sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./app.css";
-import Search from "./pages/Search";
+// import Search from "./pages/Search";
+// import CreatePlaylist from "./pages/CreatePlaylist";
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <div className="app">
-      <Search />
+      <Sidebar />
+      {/* <Search /> */}
+      {/* <CreatePlaylist /> */}
     </div>
   );
 }
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,8 +3,14 @@ import style from "./sidebar.module.css";
 
 export default function Sidebar() {
   return (
-    <div className={style.sidebar}>
-      <button> + Create Playlist</button>
+    <div className={style["wrapper-sidebar"]}>
+      <div className={style["logo"]}>
+        <h2>Music Player</h2>
+      </div>
+      <div className={style["menu"]}>
+        <button> Create Playlist</button>
+        <button> Search</button>
+      </div>
     </div>
   );
 }
